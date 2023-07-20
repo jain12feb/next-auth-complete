136c0ca0ab10d128b8bb578cc87ca9d13054abd2
jain12feb <jain00prince@gmail.com>
2023-07-21 03:05:25 +0530

    last commit

--- app/page.js
+++ app/page.js
@@ -6,17 +6,14 @@ export default function Home() {
     <main className="max-w-5xl mx-auto mt-12 px-4 text-center">
       <div className="w-full max-w-3xl mx-auto">
         <h1 className="text-4xl font-bold mt-2 mb-6">
-          Helping you find your new home;
+          NEXT-AUTH
           <br />
-          by simplifying the search.
+          Authentication for Next.js
         </h1>
         <p className="px-4 leading-relaxed">
-          Propiti provides you with a quick, simple way to reach multiple estate
-          agents.
+          with Google, Github and
           <br />
-          Allowing you to spend less time trawling through property portals,
-          <br />
-          giving you more time on the other things you enjoy.
+          User Credentials.
         </p>
         <p className="mb-8 mt-4 px-4 leading-relaxed">
           Oh, and the best bit...
@@ -27,7 +24,7 @@ export default function Home() {
             className="inline-block py-4 px-8 leading-none text-white bg-slate-800 hover:bg-pink-600 rounded shadow text-sm font-bold"
             href="/register"
           >
-            Sign-up for free
+            Get Started
           </Link>
         </div>
       </div>
